d9bce8822630c440f583d228664aea47b5f8b47e
mashalla <GKC_Alex@web.de>
2011-06-09 14:50:52 +0200

    :)

--- Source/demo.js
+++ Source/demo.js
@@ -10,20 +10,18 @@ authors:
 -Philip Fieber
 -Christian Merz
 
-version: 0.1
-
 requires:
-core/1.2.4:
-- Class
-- Class.Extras 
-- Element 
-- Element.Event
-- Selectors
-more/1.2.4:
-- Element.Delegation
+-core/1.2.4: Class
+-core/1.2.4: Class.Extras
+-core/1.2.4: Element
+-core/1.2.4: Element.Event
+-core/1.2.4: Selectors
+-more/1.2.4: Element.Delegation
 
-provides: 
-- Accessible TabPane
+provides: Accessible TabPane
+
+version: 1.0
+
 ...
 */
 document.addEvent('domready', function(){
